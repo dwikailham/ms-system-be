0fcfdbd4c8ab5e968d1dcbdc83ffe9d76b02c6d2
Dwika Ilham Zakaria <dwikailham37@gmail.com>
2025-07-30 16:37:43 +0700

    fix(route/user): change route API

--- src/routes/UserRoute.ts
+++ src/routes/UserRoute.ts
@@ -11,9 +11,9 @@ import {
 const router = express.Router();
 
 router.get("/users", accessValidation, getUsers);
-router.get("/users/:id", accessValidation, getUserById);
+router.get("/users/detail/:id", accessValidation, getUserById);
 router.post("/users", accessValidation, createUser);
-router.patch("/users/:id", accessValidation, updateUser);
-router.delete("/users/:id", accessValidation, deleteUser);
+router.patch("/users/update/:id", accessValidation, updateUser);
+router.delete("/users/delete/:id", accessValidation, deleteUser);
 
 export default router;
