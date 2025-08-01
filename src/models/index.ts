f3c7bdaddd6bf70e558980d3d89ced5960dbecb8
Dwika Ilham Zakaria <dwikailham37@gmail.com>
2025-08-01 10:49:01 +0700

    fix(models): simplify export and import models

---
+++ src/models/index.ts
@@ -0,0 +1,5 @@
+export { default as EmployeeModel } from "./EmployeeModel";
+export { default as PayDayModel } from "./PayDayModel";
+export { default as PresenceModel } from "./PresenceModel";
+export { default as UserModel } from "./UserModel";
+export { default as WorkPlacementModel } from "./WorkPlacementModel";
